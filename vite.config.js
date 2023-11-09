936cfd15539a277ddb64f46abe3e18608bafb681
gabriel.laurindo.1 <gabriel.laurindo.1@outlook.com>
2023-11-09 10:56:46 -0300

    FIX: Configurações de deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/newsletter-simple/',
+  base: '/newsletter-simple',
   plugins: [vue()],
 })
